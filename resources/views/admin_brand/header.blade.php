828bd54514de0f3afc64c31d6bc8e4c25cd04653
isnabok <isnabok@gmail.com>
2021-12-04 21:52:04 +0400

    first commit

---
+++ resources/views/admin_brand/header.blade.php
@@ -0,0 +1,7 @@
+<p class="h2 n-m font-thin v-center">
+    <x-orchid-icon path="umbrella"/>
+    <span class="m-l d-none d-sm-block">
+        Sanatorium
+        <small class="v-top opacity">test</small>
+    </span>
+</p>
